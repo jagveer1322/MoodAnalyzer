856c4207f0b80f53c2246813e4ad4a5bd73f6dc8
jagveer <jagveersingh.55555@gmail.com>
2022-04-02 16:29:37 +0530

    commit for Uc3

--- src/MoodAnalyzer.java
+++ src/MoodAnalyzer.java
@@ -1,4 +1,6 @@
 
+import exception.MoodAnalysisException;
+
 public class MoodAnalyzer {
 	private String message;
 
@@ -6,14 +8,23 @@ public class MoodAnalyzer {
 		this.message = message;
 	}
 
-	public String analyseMood() {
+	public String analyseMood(String message) throws MoodAnalysisException {
+		this.message = message;
+		return analyseMood();
+	}
+
+	public String analyseMood() throws MoodAnalysisException {
 		try {
+			if (message.length() == 0)
+				throw new MoodAnalysisException(MoodAnalysisException.exceptionType.ENTERED_EMPTY,
+						"Please enter Proper Mood");
 			if (message.contains("Sad"))
 				return "SAD";
 			else
 				return "HAPPY";
 		} catch (NullPointerException e) {
-			return "HAPPY";
+			throw new MoodAnalysisException(MoodAnalysisException.exceptionType.ENTERED_NULL,
+					"Please enter Proper Mood");
 		}
 	}
 }
